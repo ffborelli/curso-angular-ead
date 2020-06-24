c3e6b9b55f74a81e87e1c22ba43ae968d669e09b
Fabrizio Borelli <ffborelli@gmail.com>
2020-06-24 06:19:39 -0300

    hostListener e hostbinding

--- src/app/diretivas/highlight.directive.ts
+++ src/app/diretivas/highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
@@ -6,12 +6,27 @@ import { Directive, ElementRef, Renderer2 } from '@angular/core';
 export class HighlightDirective {
 
 
-  constructor( private el : ElementRef, private renderer : Renderer2 ) {
-
+  constructor(  ) {
+    //private el : ElementRef, private renderer : Renderer2
     //console.log ( this.el );
     //this.el.nativeElement.style.backgroundColor = 'yellow';
+    //this.renderer.setStyle (this.el.nativeElement, 'background-color', 'blue');
+   }
 
-    this.renderer.setStyle (this.el.nativeElement, 'background-color', 'blue');
+   @HostBinding('style.background-color')
+   private minhaCor : string;
+
+   @HostListener('mouseenter')
+   onMouseEnter(){
+    //console.log ('onMouseEnter');
+    //this.renderer.setStyle (this.el.nativeElement, 'background-color', 'blue');
+    this.minhaCor = 'blue';
+   }
+
+   @HostListener ('mouseleave')
+   onMouseLeave(){
+    //this.renderer.setStyle (this.el.nativeElement, 'background-color', 'white');
+    this.minhaCor = 'white';
    }
 
 }
